947018b7127d7936d8106906b41249e4880fdf24
MarcusSlover <mariuszek016@gmail.com>
2021-09-05 21:37:55 +0200

    + listener

--- src/main/java/me/marcusslover/resourcepackerplugin/BlockListener.java
+++ src/main/java/me/marcusslover/resourcepackerplugin/BlockListener.java
@@ -1,14 +1,25 @@
 package me.marcusslover.resourcepackerplugin;
 
+import me.marcusslover.resourcepacker.core.object.block.RPState;
+import me.marcusslover.resourcepacker.util.BlockUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
 
 public class BlockListener implements Listener {
+
+    /*Just in case this ever changes*/
+    private static final Material CUSTOM_BLOCK = Material.NOTE_BLOCK;
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlace(BlockPlaceEvent e) {
@@ -17,8 +28,23 @@ public class BlockListener implements Listener {
         Player p = e.getPlayer();
         ItemStack i = e.getItemInHand();
 
-        if (i.getType() != Material.NOTE_BLOCK) return;
+        if (i.getType() != CUSTOM_BLOCK) return;
+        ItemMeta meta = i.getItemMeta();
+        if (meta == null) return;
+        int customModelData = meta.getCustomModelData();
 
+        RPState state = BlockUtil.craftBlock(customModelData);
+        Block block = e.getBlock();
+        List<RPState.Element> list = state.list();
 
+        String id = "minecraft:" + CUSTOM_BLOCK.toString().toLowerCase();
+        StringBuilder builder = new StringBuilder();
+        for (RPState.Element element : list) {
+            builder.append(element.key).append("=").append(element.value).append(",");
+        }
+        String states = builder.toString();
+        String data = id + "[" + states.substring(0, states.length() - 1) + "]";
+        BlockData blockData = Bukkit.createBlockData(data);
+        block.setBlockData(blockData);
     }
 }
--- src/main/java/me/marcusslover/resourcepackerplugin/ResourcePackerPlugin.java
+++ src/main/java/me/marcusslover/resourcepackerplugin/ResourcePackerPlugin.java
@@ -2,6 +2,7 @@ package me.marcusslover.resourcepackerplugin;
 
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -10,10 +11,15 @@ public class ResourcePackerPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        loadCommand("itemdata", new ItemDataCommand());
+        registerListener(new BlockListener());
+        registerCommand("itemdata", new ItemDataCommand());
     }
 
-    private void loadCommand(@NotNull String label, @Nullable CommandExecutor executor) {
+    private void registerListener(@NotNull Listener listener) {
+        getServer().getPluginManager().registerEvents(listener, this);
+    }
+
+    private void registerCommand(@NotNull String label, @Nullable CommandExecutor executor) {
         if (executor == null) return;
         PluginCommand command = getCommand(label);
         if (command == null) return;
